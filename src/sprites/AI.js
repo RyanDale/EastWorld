bd4b5f68a758ea91517deb8f232d08c66ade6cf0
Ryan Dale <rd01748@georgiasouthern.edu>
2017-01-26 12:20:35 -0500

    Break out Player into Human base class. Add the ability to kill AI.

--- src/sprites/AI.js
+++ src/sprites/AI.js
@@ -1,14 +1,18 @@
 import {Phaser, Point, Rectangle} from 'phaser'
-import Player from './Player'
 import Waypoint from '../classes/Waypoint'
+import Human from './Human'
 
-export default class AI extends Player {
+export default class AI extends Human {
 
     constructor(game, asset, frame = null) {
         let position = Waypoint.randomWalkable().position;
         super(game, position.x, position.y, asset, frame);
         this.anchor.setTo(0.5);
         this.generateNewTarget();
+        if (!AI.ai) {
+            AI.ai = [];
+        }
+        AI.ai.push(this);
     }
 
     movePlayer() {
@@ -28,7 +32,7 @@ export default class AI extends Player {
 
         _.each(wayPoints, wayPoint => {
             distance = Point.distance(wayPoint.position, this.target.position);
-            if (distance < currentDistance) {
+            if (distance <= currentDistance) {
                 currentDistance = distance;
                 currentWayPoint = wayPoint;
             }
@@ -38,11 +42,11 @@ export default class AI extends Player {
 
     /* Find points surrounding the AI */
     findPoints() {
-        const spacing = 512;
-        let rectangle1 = new Rectangle(this.position.x, this.position.y + spacing, this.width, this.height),
-            rectangle2 = new Rectangle(this.position.x + spacing, this.position.y, this.width, this.height),
-            rectangle3 = new Rectangle(this.position.x, this.position.y - spacing, this.width, this.height),
-            rectangle4 = new Rectangle(this.position.x - spacing, this.position.y, this.width, this.height);
+        const SPACING = 512;
+        let rectangle1 = new Rectangle(this.position.x, this.position.y + SPACING, this.width, this.height),
+            rectangle2 = new Rectangle(this.position.x + SPACING, this.position.y, this.width, this.height),
+            rectangle3 = new Rectangle(this.position.x, this.position.y - SPACING, this.width, this.height),
+            rectangle4 = new Rectangle(this.position.x - SPACING, this.position.y, this.width, this.height);
         return _.filter(Waypoint.wayPoints, wayPoint => rectangle1.contains(wayPoint.position.x, wayPoint.position.y) ||
             rectangle2.contains(wayPoint.position.x, wayPoint.position.y) ||
             rectangle3.contains(wayPoint.position.x, wayPoint.position.y) ||
@@ -61,7 +65,7 @@ export default class AI extends Player {
         if (new Rectangle(this.wayPoint.position.x - 4, this.wayPoint.position.y - 4, 8, 8).contains(this.position.x,
                 this.position.y)) {
             this.position = _.clone(this.wayPoint.position);
-            this.wayPoint = this.findDistance(this.findPoints());
+            this.wayPoint = this.findDistance(this.findPoints().filter(point => point.position.x !== this.position.x || point.position.y !== this.position.y));
             if (this.position.x == this.target.position.x && this.position.y == this.target.position.y) {
                 this.generateNewTarget();
             }
--- src/sprites/Human.js
+++ src/sprites/Human.js
@@ -1,8 +1,6 @@
 import Phaser from 'phaser'
-import AI from './AI'
-import Human from './Human'
 
-class Player extends Human {
+class Human extends Phaser.Sprite {
     constructor(game, x, y, asset, frame) {
         super(game, x, y, asset, frame);
         this.activeWeapon = null;
@@ -53,52 +51,6 @@ class Player extends Human {
             this.animations.play('walking', 8, repeat);
         }
     }
-
-    switchWeapon(weaponName) {
-        this.activeWeapon.name = _.find(this.weaponList, name => name === weaponName);
-        this.frame = this.animations.getAnimation(weaponName).frame;
-        this.weapon = this.game.add.weapon(-1, `${this.activeWeapon.name}Bullet`);
-        this.weapon.physicsBodyType = Phaser.Physics.ARCADE;
-        this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-        this.weapon.bulletSpeed = 600;
-        this.weapon.fireRate = 100;
-        this.weapon.bulletCollideWorldBounds = true;
-        this.weapon.bulletAngleOffset = 90;
-        this.weapon.bulletAngleVariance = 5;
-        this.weapon.enableBody = true;
-
-    }
-
-    cycleWeapon() {
-        if (_.get(this, 'activeWeapon.name')) {
-            let index = _.findIndex(this.weaponList, name => name === this.activeWeapon.name);
-            if (index + 1 < this.weaponList.length) {
-                this.switchWeapon(this.weaponList[index + 1]);
-            } else {
-                this.switchWeapon(this.weaponList[0]);
-            }
-        } else {
-            this.switchWeapon(this.weaponList[0]);
-        }
-    }
-
-    shootWeapon() {
-        if (!this.activeWeapon.name) {
-            return;
-        }
-        this.weapon.fireAngle = this.game.math.radToDeg(this.body.rotation) - 90;
-        this.weapon.trackSprite(this, 0, 0);
-        this.bullet = this.weapon.fire();
-    }
-
-    update() {
-        if (this.weapon) {
-            this.game.physics.arcade.overlap(this.weapon.bullets, AI.ai, (bullet, enemy) => {
-                bullet.kill();
-                enemy.kill();
-            });
-        }
-    }
 }
 
-export default Player;
+export default Human;
